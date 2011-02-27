07712227108acfec7208b0e0eaa0bbb7bb8572e7
Alexander Dorofeev <aka.spin@gmail.com>
2011-02-27 16:21:47 +0500

    fix error handling

--- src/roller.erl
+++ src/roller.erl
@@ -6,10 +6,11 @@
 new(Error)->
     instance(Error).
 new()->
-    Err = fun(Code, Reason)-> 
+    Err = fun(Code, Reason, Class)-> 
                   {Code, 
-                   [<<"Content-Type">>,<<"text/plain; charset=utf-8">>], 
-                   mochifmt:bformat("{0}\n{1}", [Code, Reason])}
+                   [{<<"Content-Type">>,<<"text/plain; charset=utf-8">>}], 
+                   mochifmt:bformat(
+                    "{0} ({1}): {2}", [Code, Class, Reason])}
           end,
     instance(Err).
 
@@ -18,13 +19,13 @@ roll(Request, Args, Chain)->
     do(Request, Args, Chain).
 
 do(Request, Args, [])->
-   case Args of
-       ok -> 
-           ok;
-       Err -> 
-           % Some wrong here
-           send_error(Request, 500, {"End of chain", Err})
-   end;
+    case Args of
+        ok -> 
+            ok;
+        NonClosed -> 
+            % Some wrong here
+            send_error(Request, 500, {"Non ended chain", NonClosed}, "Error")
+    end;
 
 do(Request, Args, [Current|Rest])->
     Handler = Current:new(Request),
@@ -32,14 +33,16 @@ do(Request, Args, [Current|Rest])->
         ok -> ok;
         Ret -> do(Request, Ret, Rest)
     catch
-        error:{Code, Reason}->
-            send_error(Request, Code, Reason);
+        Class:{Code, Reason} when is_integer(Code)->
+            send_error(Request, Code, Reason, Class);
             
         Class:Err->
             Rem = integer_to_list(length(Rest)),
             send_error(Request, 500, 
-                       {"Error. " ++ Rem ++ " steps left.", {Class, Err}})
+                    {"Error. " ++ Rem ++ " steps left.", Err}, Class)
     end.
 
-send_error(Request, Code, Reason)->
-    Request:respond(Error(Code, Reason)).
+send_error(Request, Code, Reason, Class)->
+    error_logger:error_report(["Roller flow error", {class, Class},
+                               {code, Code}, {reason, Reason}]),
+    Request:respond(Error(Code, Reason, Class)).
